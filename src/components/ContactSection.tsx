21390d5fdfc4c7ea110382ad5689ab97c2d95c97
Yuriy Koshyk <yuriy.koshyk@gmail.com>
2024-12-07 23:03:06 +0100

    commit

--- src/components/ContactSection.tsx
+++ src/components/ContactSection.tsx
@@ -1,10 +1,30 @@
+import Link from "next/link";
+
 const ContactSection = () => {
   return (
-    <section
-      id="contact"
-      className="flex items-center justify-center w-full h-full"
-    >
-      Contacts
+    <section id="contact" className="grid grid-cols-2 w-full h-full">
+      <div className="col-start-2 flex flex-col gap-6 justify-center">
+        Feel free to contact me via:
+        <ul>
+          <li>
+            Email: <Link href={"/"}>anyaa.segeda@gmail.com</Link>
+          </li>
+          <li>
+            Telegram: <Link href={"/"}>@anna_sehedaa</Link>
+          </li>
+          <li>
+            Instagram:{" "}
+            <ul className="inline-flex flex-col">
+              <li>
+                <Link href={"/"}>@seheda_anya</Link>
+              </li>
+              <li>
+                <Link href={"/"}>@sehedanna</Link>
+              </li>
+            </ul>
+          </li>
+        </ul>
+      </div>
     </section>
   );
 };
